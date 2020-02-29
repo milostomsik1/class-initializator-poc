4ae044aa5ce029899f34365a4403ed703aa382e4
Miloš Tomšik <milostomsik@Miloss-MacBook-Pro.local>
2020-02-29 22:48:10 +0100

    different implementations

--- app.ts
+++ app.ts
@@ -1,26 +1,33 @@
-class InitObject<T> {
-    constructor(initializator?: Partial<T>) {
-        if (initializator) {
-            Object.assign(this, initializator);
+class BaseObject<T> {
+    constructor(initializer?: Partial<T>) {
+        if (initializer) {
+            Object.assign(this, initializer);
         }
     }
 }
 
-class User extends InitObject<User> {
+class User extends BaseObject<User> {
     firstName: string;
     lastName: string;
     age: number;
-
-    constructor(initializator?: Partial<User>) {
-        super(initializator);
-    }
 
     sayMyName() {
         console.log(`${this.firstName} ${this.lastName}`);
     }
 }
 
+class Item extends BaseObject<Item> {
+    name: string;
+
+    constructor(actualCtorDep: string, initializer?: Partial<Item>) {
+        super(initializer);
+    }
+}
+
 const user1 = new User({age: 32, firstName: 'Pera', lastName: 'Peric'});
+const item1 = new Item('asdf', { name: 'item1' });
 
 console.log(user1);
 user1.sayMyName();
+
+console.log(item1);
